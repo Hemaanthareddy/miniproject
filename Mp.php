4c7f5545b533fad99e9f5d7b4010fa269cdd66d5
hemalatha2000301 <57004454+hemalatha2000301@users.noreply.github.com>
2020-11-17 08:00:39 -0800

    Add files via upload

---
+++ Mp.php
@@ -0,0 +1,31 @@
+<?php
+	$servername="localhost";
+	$username="root"; 
+	$password=""; 
+	$dbname="SRM";
+	$connect=MySQLi_connect($servername,$username,$password) or die("could not connect");
+	if($connect)
+		echo"done";
+	else
+		echo"not";
+	$db=mysqli_select_db($connect,$dbname);
+	If($db)
+		echo"yes";
+	include("pro 1.php");
+	if(isset($_REQUEST["submit"]))
+	{
+	@$username=$_REQUEST["username"];
+	@$password=$_REQUEST["password"];
+	$extract=mysqli_query($connect,"select * from admin where UserName='$username'&& Password='$password'");
+	$rowcount=mysqli_num_rows($extract);
+	if($rowcount==true)
+	{
+		 header("location:Ban1.php");
+	}
+	else
+	{
+		echo "rong";
+	}
+	}
+		
+?>
